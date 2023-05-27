8629b8c81327c7d8ed313c7c1fe503480110a294
NewDaci <ai.ranjt@gmail.com>
2023-05-27 15:45:51 +0530

    class error fix

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -1,11 +1,11 @@
 
 function HeroSection() {
     return (
-            <section class="text-gray-600 body-font">
-                <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-                    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
-                        <div class="text-center lg:w-2/3 w-full">
-                             <div class="flex justify-center">
+            <section className="text-gray-600 body-font">
+                <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                    <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+                        <div className="text-center lg:w-2/3 w-full">
+                             <div className="flex justify-center">
                                
                             </div>
                         </div>
